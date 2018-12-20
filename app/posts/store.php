b33c3c074e9dc7529acef8fa31ec8065a68ca247
Benjamin Fransson <benjamin@cafcon.se>
2018-12-20 17:48:43 +0100

    Added basic logic to insert posts

--- app/posts/store.php
+++ app/posts/store.php
@@ -6,4 +6,27 @@ require __DIR__.'/../autoload.php';
 
 // In this file we store/insert new posts in the database.
 
+if(USER_LOGGEDIN && isset($_POST[''], $_POST[''])){
+    $query = 'INSERT INTO posts (users_id, image, content) VALUES (:user_id, :image, :content);';
+
+    $stmt = $pdo->prepare($query);
+
+    $result = $stmt->execute($stmt, [
+        ':user_id' => $_SESSION['user'][id],
+        ':image' => '',
+        ':content' => ''
+    ]);
+
+    if(!$result){
+        $mess = 'EITHER: you are not logged in OR: some form inputs went missing';
+
+        $_SESSION['messages'][] = $mess;
+    }
+
+}else{
+    $mess = 'EITHER: you are not logged in OR: some form inputs went missing';
+
+    $_SESSION['messages'][] = $mess;
+}
+
 redirect('/');
