39b891784cb80fa45e590f3b67daead6ba35a0ad
Benjamin Fransson <benjamin@cafcon.se>
2018-12-21 02:34:45 +0100

    Added registration form

--- register.php
+++ register.php
@@ -1,8 +1,18 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+    <h1>Register</h1>
+    <form action="/app/users/register.php" method="post">
+        <label for="name">Name:</label>
+        <input name="name" class="form-control" type="text">
+        <label for="email">Email:</label>
+        <input name="email" class="form-control" type="email">
+        <label for="password">Password:</label>
+        <input name="password" class="form-control" type="password">
+        <label for="c_password">Confirm password:</label>
+        <input name="c_password" class="form-control" type="password">
+        <input type="submit" class="btn btn-primary mt-4" value="Register">
+    </form>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
