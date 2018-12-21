6b51b59d757f300a186e56ebf29c2bf2c63c99ea
Benjamin Fransson <benjamin@cafcon.se>
2018-12-21 03:19:58 +0100

    Added login logic

--- app/users/login.php
+++ app/users/login.php
@@ -5,3 +5,26 @@ declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
 // In this file we login users.
+if(isset($_POST['email'], $_POST['password'])){
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+
+    $query = 'SELECT * FROM users WHERE email=:email;';
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([
+       ':email' => $email
+    ]);
+    $result = $stmt->fetch();
+
+    if(password_verify($password, $result['password'])){
+        $_SESSION['user'] = $result;
+
+        set_alert('Registration successfull!', 'success');
+        redirect('/');
+    }
+        set_alert('Login failed: Email or password was invalid.', 'danger');
+        redirect('/');
+}
+
+set_alert('Something went wrong, please try again!', 'warning');
+redirect('/');
