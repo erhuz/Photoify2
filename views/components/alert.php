6cd43eb7a0de57a92796622faf94812523a6d7be
Benjamin Fransson <benjamin@cafcon.se>
2018-12-21 03:25:24 +0100

    Added an alert component to views

---
+++ views/components/alert.php
@@ -0,0 +1,7 @@
+<div class="row">
+    <div class="col">
+        <div class="alert alert-<?= $alert['type'] ?>" role="alert">
+            <?= $alert['content'] ?>
+        </div>
+    </div>
+</div>
