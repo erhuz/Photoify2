c37623432dc8b9ddc0a8f06388bebd29b9a3eab7
Benjamin Fransson <benjamin@cafcon.se>
2018-12-21 03:25:05 +0100

    Added conditional routes for guests & logged in users.

--- views/navigation.php
+++ views/navigation.php
@@ -1,13 +1,29 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+    <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
 
-  <ul class="navbar-nav">
-      <li class="nav-item">
-          <a class="nav-link" href="/index.php">Home</a>
-      </li><!-- /nav-item -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/index.php">Home</a>
+        </li><!-- /nav-item -->
 
-      <li class="nav-item">
-          <a class="nav-link" href="/about.php">About</a>
-      </li><!-- /nav-item -->
-  </ul><!-- /navbar-nav -->
+        <li class="nav-item">
+            <a class="nav-link" href="/about.php">About</a>
+        </li><!-- /nav-item -->
+        <?php if(USER_LOGGEDIN): ?>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/app/users/logout.php">Log out</a>
+            </li><!-- /nav-item -->
+
+        <?php else: ?>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/login.php">Login</a>
+            </li><!-- /nav-item -->
+
+            <li class="nav-item">
+                <a class="nav-link" href="/register.php">Register</a>
+            </li><!-- /nav-item -->
+        <?php endif; ?>
+    </ul><!-- /navbar-nav -->
 </nav><!-- /navbar -->
