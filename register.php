3b6213f7fe8b319d48a9fabb19b5047219185e93
Benjamin Fransson <benjamin@cafcon.se>
2018-12-21 02:46:46 +0100

    Added spacing for better readability

--- register.php
+++ register.php
@@ -5,12 +5,16 @@
     <form action="/app/users/register.php" method="post">
         <label for="name">Name:</label>
         <input name="name" class="form-control" type="text">
+
         <label for="email">Email:</label>
         <input name="email" class="form-control" type="email">
+
         <label for="password">Password:</label>
         <input name="password" class="form-control" type="password">
+
         <label for="c_password">Confirm password:</label>
         <input name="c_password" class="form-control" type="password">
+
         <input type="submit" class="btn btn-primary mt-4" value="Register">
     </form>
 </article>
