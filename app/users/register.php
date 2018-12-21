7982900bd08ea4a376a05115d6884fb73c87ad70
Benjamin Fransson <benjamin@cafcon.se>
2018-12-21 03:20:06 +0100

    Added register logic

--- app/users/register.php
+++ app/users/register.php
@@ -5,3 +5,30 @@ declare(strict_types=1);
 require __DIR__.'/../autoload.php';
 
 // In this file we register users.
+
+if(isset($_POST['name'], $_POST['email'], $_POST['password'], $_POST['c_password'])){
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
+    $c_password = filter_var($_POST['c_password'], FILTER_SANITIZE_STRING);
+
+    if($password !== $c_password){
+        set_alert('Passwords did not match.', 'warning');
+        redirect('/');
+    }
+
+    $query = 'INSERT INTO users (name, email, password) VALUES (:name, :email, :password);';
+    $stmt = $pdo->prepare($query);
+    $stmt->execute([
+       ':name' => $name,
+       ':email' => $email,
+       ':password' => password_hash($password)
+    ]);
+
+    set_alert('Registration successfull!', 'success');
+    redirect('/');
+}
+
+
+set_alert('Something went wrong, please try again!', 'danger');
+redirect('/');
