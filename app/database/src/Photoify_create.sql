d2fcfd24b95d894b561834eb37d42cef33a86b11
Benjamin Fransson <benjamin@cafcon.se>
2018-12-23 00:07:05 +0100

    Converted MYSQL to SQLite acceptable code.

--- app/database/src/Photoify_create.sql
+++ app/database/src/Photoify_create.sql
@@ -1,47 +1,45 @@
 -- tables
+
+-- Table: users
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    password varchar(255) NOT NULL,
+    avatar varchar(511),
+    created_at TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP
+    -- updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Table: posts
+CREATE TABLE posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    users_id INTEGER NOT NULL,
+    image varchar(511) NOT NULL,
+    content varchar(511) NOT NULL,
+    created_at TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP,
+    -- updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(users_id) REFERENCES users(id)
+);
+
 -- Table: comments
 CREATE TABLE comments (
-    id int  NOT NULL,
-    users_id int  NOT NULL,
-    posts_id int  NOT NULL,
-    created_at timestamp  NOT NULL,
-    updated_at timestamp  NOT NULL,
-    CONSTRAINT comments_pk PRIMARY KEY (id)
-    FOREIGN KEY(users_id) REFERENCES users(id)
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    users_id INTEGER NOT NULL,
+    posts_id INTEGER NOT NULL,
+    created_at TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP,
+    -- updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(users_id) REFERENCES users(id),
     FOREIGN KEY(posts_id) REFERENCES posts(id)
 );
 
 -- Table: likes
 CREATE TABLE likes (
-    users_id int  NOT NULL,
-    posts_id int  NOT NULL,
-    created_at timestamp  NOT NULL,
-    updated_at timestamp  NOT NULL,
+    users_id INTEGER NOT NULL,
+    posts_id INTEGER NOT NULL,
+    created_at TIMESTAMP DATETIME DEFAULT CURRENT_TIMESTAMP,
+    -- updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     CONSTRAINT likes_pk PRIMARY KEY (users_id,posts_id)
     FOREIGN KEY(users_id) REFERENCES users(id)
     FOREIGN KEY(posts_id) REFERENCES posts(id)
-);
-
--- Table: posts
-CREATE TABLE posts (
-    id int  NOT NULL,
-    users_id int  NOT NULL,
-    image varchar(511)  NOT NULL,
-    content varchar(511)  NOT NULL,
-    created_at timestamp  NOT NULL,
-    updated_at timestamp  NOT NULL,
-    CONSTRAINT posts_pk PRIMARY KEY (id)
-    FOREIGN KEY(users_id) REFERENCES users(id)
-);
-
--- Table: users
-CREATE TABLE users (
-    id int  NOT NULL,
-    name varchar(255)  NOT NULL,
-    email varchar(255)  NOT NULL,
-    password varchar(255)  NOT NULL,
-    avatar varchar(511)  NOT NULL,
-    created_at timestamp  NOT NULL,
-    updated_at timestamp  NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id)
 );
