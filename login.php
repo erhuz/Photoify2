2fd2a0491362c498cd1fafe7c981cb9b76ea5ecd
Benjamin Fransson <benjamin@cafcon.se>
2018-12-23 00:25:51 +0100

    Added login form & page variable for active link

--- login.php
+++ login.php
@@ -1,8 +1,18 @@
+<?php $page = 'login'; ?>
+
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+    <h1>Login</h1>
+    <form action="/app/users/login.php" method="post">
+        <label for="email">Email:</label>
+        <input name="email" class="form-control" type="email">
+
+        <label for="password">Password:</label>
+        <input name="password" class="form-control" type="password">
+
+        <input type="submit" class="btn btn-primary mt-4" value="Login">
+    </form>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
