681b463fed805a36c2a73ddab2ab1d4c3d0c0286
Benjamin Fransson <benjamin@cafcon.se>
2018-12-23 03:27:09 +0100

    Added the ground for a profile page

---
+++ profile.php
@@ -0,0 +1,10 @@
+<?php $page = 'profile'; ?>
+
+<?php require __DIR__.'/views/header.php'; ?>
+
+<article>
+    <h1><?= User['name'] ?></h1>
+
+</article>
+
+<?php require __DIR__.'/views/footer.php'; ?>
