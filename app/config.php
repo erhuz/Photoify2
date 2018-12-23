7be0d04b46ee81964fa27e94f1704a5a2edf7a82
Benjamin Fransson <benjamin@cafcon.se>
2018-12-23 02:33:24 +0100

    Added Project title

--- app/config.php
+++ app/config.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 // This file contains a list of global configuration settings.
 
 return [
-    'title' => 'Project',
+    'title' => 'Photoify',
     'database_path' => sprintf('sqlite:%s/database/photoify.db', __DIR__),
 ];
