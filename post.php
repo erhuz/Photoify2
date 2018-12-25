ff9ebe46397c8be7a01a788b1b0e31e4e0662743
Benjamin Fransson <benjamin@cafcon.se>
2018-12-25 23:37:54 +0100

    Added post create form

--- post.php
+++ post.php
@@ -1,20 +1,38 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<article>
-    <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+<article class="row">
+    <h1>New post</h1>
 </article>
 
 <article>
-    <form action="/app/posts/store.php" method="post" enctype="multipart/form-data">
-        <label for="image">Picture</label>
-        <input type="file" name="image" id="image">
+    <section class="row justify-content-center">
+    <div class="card-image">
+        <img class="" src="<?= $post['description'] ?>" alt="Card image cap">
+    </div>
+    </section>
+    <section class="row justify-content-center">
+        <div class="col-md-6">
+            <form action="/app/posts/store.php" method="post" enctype="multipart/form-data">
+                <label class="mt-4" for="image">Picture</label>
+                <input type="file" name="image" id="image" required>
 
-        <label for="content">Description / Content</label>
-        <textarea name="content" id="content" cols="20" rows="10"></textarea>
+                <label class="mt-4" for="content">Description / Content</label>
+                <textarea
+                    class="form-control"
+                    name="content"
+                    id="content"
+                    cols="20"
+                    rows="5"
+                    maxlength="128"
+                    placeholder="Type something in your post!"
+                    required
+                ></textarea>
 
-        <input type="submit" value="Upload">
-    </form>
+                <input type="submit" class="btn btn-primary btn-block mt-2" value="Upload">
+            </form>
+        </div>
+    </section>
+
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
