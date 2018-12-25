935efe13243a167cc4d5c200856686939b172801
Benjamin Fransson <benjamin@cafcon.se>
2018-12-25 23:40:18 +0100

    Added post component

---
+++ views/components/post.php
@@ -0,0 +1,28 @@
+<section class="col-md-10 col-lg-6">
+    <div class="row post">
+        <div class="col">
+            <div class="card">
+
+                <div class="card-header">
+                    <div class="profile-picture-container">
+                        <img class="profile-picture rounded-circle" src="<?= '/uploads/avatars/' . $post['avatar'] ?>"
+                            alt="">
+                    </div>
+                    <h5 class="card-title"><a href="<?= '/posts.php?user=' . $post['user_id'] ?>">
+                            <?= $post['name'] ?></a></h5>
+                </div>
+
+                <a class="card-image" href="#">
+                    <img class="" src="<?= $post['image'] ?>" alt="Card image cap">
+                </a>
+
+                <div class="card-body">
+                    <div class="card-text">
+                        <p><?= $post['description'] ?></p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
