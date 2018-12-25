0e2604f8a39c09803f1cb38d7cda3d0da0723d38
Benjamin Fransson <benjamin@cafcon.se>
2018-12-25 04:44:35 +0100

    Added name, email & bio form, password reset & avatar change func

--- profile.php
+++ profile.php
@@ -2,9 +2,105 @@
 
 <?php require __DIR__.'/views/header.php'; ?>
 
-<article>
-    <h1><?= User['name'] ?></h1>
-
+<article class="row">
+    <div class="col">
+        <h1>Profile</h1>
+        <p>Here you can edit all stored information about your account.</p>
+    </div>
 </article>
+
+<div class="row user-profile">
+    <div class="col">
+
+        <div class="row">
+            <div class="col">
+                <h2>Account information</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12 col-lg-8">
+                <form action="/app/users/update.php" method="POST">
+
+                    <label class="mt-3" for="name">Name</label>
+                    <input class="form-control ds-input" type="text" name="name" value="<?= User['name'] ?>">
+
+                    <label class="mt-3" for="email">E-mail</label>
+                    <input class="form-control ds-input" type="email" name="email" value="<?= User['email'] ?>">
+
+                    <label class="mt-3" for="bio">Biography</label>
+                    <textarea class="form-control ds-input" name="bio" id="bio" cols="30" rows="7" placeholder="Tell people who you are!"><?= User['bio'] ?></textarea>
+
+                    <input class="btn btn-primary mt-3" type="submit" value="Save">
+                </form>
+            </div>
+
+            <form action="/app/users/update.php" method="POST" enctype="multipart/form-data" class="profile-img-form">
+                <label for="image">Change avatar</label>
+                <input type="file" name="avatar" id="image" accept="image/x-png,image/gif,image/jpeg" required>
+
+                <input type="submit" value="Update">
+            </form>
+
+            <div class="col-md-12 col-lg-4 profile-img-container">
+                <p class="description">Avatar updates as soon as you choose an image.
+                    <span class="img-change">Click it to change!</span>
+                </p>
+
+                <div
+                    class="inner-profile-img-container"
+                    style="background-image: url('<?=
+                    (User['avatar'] !== '')
+                    ? '/uploads/avatars/' . User['avatar']
+                    : 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png'
+                    ?>')"
+                >
+                    <div class="middle">
+                        <div class="text">Change</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="row user-profile mt-4 section-2">
+    <div class="col-md-12 col-lg-4 mt-4 manage-posts">
+        <div class="row">
+            <div class="col">
+                <h2>Other</h2>
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col">
+                <a href="#" class="btn btn-primary btn-block">Create new post</a>
+                <a href="#" class="btn btn-primary btn-block">Manage my posts</a>
+                <a href="#" class="btn btn-danger btn-block">Delete account</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-12 col-lg-8 mt-4">
+        <div class="row">
+            <div class="col">
+                <h2>Reset password</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form action="/app/users/update.php" method="POST">
+
+                    <label class="mt-3" for="password">Password</label>
+                    <input class="form-control ds-input" type="password" name="password" id="password">
+
+                    <label class="mt-3" for="c_password">Confirm password</label>
+                    <input class="form-control ds-input" type="password" name="c_password" id="c_password">
+
+                    <input class="btn btn-primary mt-3" type="submit" value="Save">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php require __DIR__.'/views/footer.php'; ?>
