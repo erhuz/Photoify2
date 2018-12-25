a0dc4d70974860edf37875e9d85382284390cda4
Benjamin Fransson <benjamin@cafcon.se>
2018-12-25 04:44:51 +0100

    Added JS for the avatar form

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,25 @@
 'use strict';
 
-console.log('Hello World');
+// Scripts for the profile-page
+(function(){
+  if(document.querySelector('.user-profile')){
+    const clickableText            = document.querySelector('.user-profile .img-change');
+    const innerProfileImgContainer = document.querySelector('.user-profile .profile-img-container .inner-profile-img-container');
+    const profileImgForm           = document.querySelector('.user-profile .profile-img-form');
+    let   profileImgInput          = document.querySelector('.user-profile #image');
+    const profileImg               = document.querySelector('.profile-img-container img');
+
+
+    clickableText.addEventListener("click", () => openProfileImgForm());
+    innerProfileImgContainer.addEventListener("click", () => openProfileImgForm());
+
+    function openProfileImgForm(){
+      profileImgInput.click();
+    };
+
+    profileImgForm.addEventListener("change", () => {
+      profileImgForm.submit();
+    });
+
+  }
+})();
