2fd9e65f2965b2f778b24b326a6c46669e9ae37c
Benjamin Fransson <benjamin@cafcon.se>
2019-01-01 03:56:19 +0100

    Added like api to handle likes

--- api/like.php
+++ api/like.php
@@ -1,11 +1,36 @@
 <?php
 declare (strict_types = 1);
 
-require __DIR__.'/../autoload.php';
+require __DIR__.'/../app/autoload.php';
 
-// In this file we like posts.
+// In this file we like posts and send the data back as encoded JSON.
+if(!USER_IS_LOGGEDIN){
+    $data = ['result' => false];
 
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    die;
+}
+
+$post_id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
+$status = filter_var($_GET['status'], FILTER_SANITIZE_NUMBER_INT);
+
+$query = 'INSERT INTO likes (user_id, post_id, status) VALUES (:user_id, :post_id, :status);';
+$params = [
+    ':user_id' => User['id'],
+    ':post_id' => $post_id,
+    ':status' => $status
+];
+
+$stmt = $pdo->prepare($query);
+$result = $stmt->execute($params);
+
+// REMOVE ME BEFORE PRODUCTION
 if(!$stmt){
- // REMOVE ME BEFORE PRODUCTION
     die(var_dump($pdo->errorInfo()));
 }
+
+$data = ['result' => true];
+
+header('Content-Type: application/json');
+echo json_encode($data);
