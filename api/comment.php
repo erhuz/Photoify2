7f27fe86989ebcded658a35f91139679480b9d1d
Benjamin Fransson <benjamin@cafcon.se>
2019-01-01 03:55:51 +0100

    Added placeholder file for future comment api

--- api/comment.php
+++ api/comment.php
@@ -2,10 +2,14 @@
 declare (strict_types = 1);
 
 require __DIR__.'/../autoload.php';
+header('Content-Type: application/json');
 
-// In this file we comment on posts.
+// In this file we comment on posts and send the data back as encoded JSON.
+
 
 if(!$stmt){
- // REMOVE ME BEFORE PRODUCTION
+    // REMOVE ME BEFORE PRODUCTION
     die(var_dump($pdo->errorInfo()));
 }
+
+echo json_encode($data);
