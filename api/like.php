8a9271b66963ca40460dbcf8b650a46d60824ca0
Benjamin Fransson <benjamin@cafcon.se>
2019-01-01 05:40:11 +0100

    Added function to remove like if clicked upon when already liked

--- api/like.php
+++ api/like.php
@@ -15,22 +15,65 @@ if(!USER_IS_LOGGEDIN){
 $post_id = filter_var($_GET['id'], FILTER_SANITIZE_NUMBER_INT);
 $status = filter_var($_GET['status'], FILTER_SANITIZE_NUMBER_INT);
 
-$query = 'INSERT INTO likes (user_id, post_id, status) VALUES (:user_id, :post_id, :status);';
+// Check if row exists in table.
+$query = 'SELECT 1 FROM likes WHERE post_id = :post_id AND user_id = :user_id; AND status=:status';
 $params = [
-    ':user_id' => User['id'],
     ':post_id' => $post_id,
+    ':user_id' => User['id'],
     ':status' => $status
 ];
 
 $stmt = $pdo->prepare($query);
-$result = $stmt->execute($params);
+$stmt->execute($params);
+$results = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// REMOVE ME BEFORE PRODUCTION
-if(!$stmt){
-    die(var_dump($pdo->errorInfo()));
+if(isset($results[1])){
+    $liked = true;
+}else{
+    $liked = false;
 }
 
-$data = ['result' => true];
+
+if($liked){
+    $query = 'DELETE FROM likes WHERE user_id=:user_id AND post_id=:post_id AND status=:status;';
+    $params = [
+        ':user_id' => User['id'],
+        ':post_id' => $post_id,
+        ':status' => $status
+    ];
+    $stmt = $pdo->prepare($query);
+
+    // REMOVE ME BEFORE PRODUCTION
+    if(!$stmt){
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $result = $stmt->execute($params);
+
+    $data = ['result' => 'removed'];
+
+    header('Content-Type: application/json');
+    echo json_encode($data);
+    die;
+
+}else{
+    $query = 'INSERT INTO likes (user_id, post_id, status) VALUES (:user_id, :post_id, :status);';
+    $params = [
+        ':user_id' => User['id'],
+        ':post_id' => $post_id,
+        ':status' => $status
+    ];
+
+    $stmt = $pdo->prepare($query);
+    $result = $stmt->execute($params);
+
+    // REMOVE ME BEFORE PRODUCTION
+    if(!$stmt){
+        die(var_dump($pdo->errorInfo()));
+    }
+}
+
+$data = ['result' => 'inserted'];
 
 header('Content-Type: application/json');
 echo json_encode($data);
