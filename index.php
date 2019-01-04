3b3b3c0fb80b67f428fce7fad825606066da553b
Benjamin Fransson <benjamin@cafcon.se>
2019-01-04 20:41:45 +0100

    Renamed likes table to reactions

--- index.php
+++ index.php
@@ -11,13 +11,13 @@ $query = 'SELECT
         posts.created_at,
         users.name,
         users.avatar,
-        ifnull((SELECT COUNT(likes.status) WHERE likes.status = 1), 0 ) as likeCount,
-        ifnull((SELECT COUNT(likes.status) WHERE likes.status = 0), 0 ) as dislikeCount
+        ifnull((SELECT COUNT(reactions.status) WHERE reactions.status = 1), 0 ) as likeCount,
+        ifnull((SELECT COUNT(reactions.status) WHERE reactions.status = 2), 0 ) as dislikeCount
 
     FROM posts
     JOIN users ON posts.user_id = users.id
-    LEFT OUTER JOIN likes ON posts.id = likes.post_id
-    GROUP BY posts.id, users.id, likes.status
+    LEFT OUTER JOIN reactions ON posts.id = reactions.post_id
+    GROUP BY posts.id, users.id, reactions.status
     ORDER BY posts.created_at DESC;';
 
     $stmt = $pdo->prepare($query);
