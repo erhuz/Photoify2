36aed698194b75e50e8793d8e279871e889d969b
Benjamin Fransson <benjamin@cafcon.se>
2019-01-05 20:05:59 +0100

    formatted file & added delete btn logic + delete modal

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,20 @@
 'use strict';
 
-(function(){
+(function () {
 
   // Scripts for the profile-page
-  if(document.querySelector('.user-profile')){
-    const clickableText            = document.querySelector('.user-profile .img-change');
+  if (document.querySelector('.user-profile')) {
+    const clickableText = document.querySelector('.user-profile .img-change');
     const innerProfileImgContainer = document.querySelector('.user-profile .profile-img-container .inner-profile-img-container');
-    const profileImgForm           = document.querySelector('.user-profile .profile-img-form');
-    let   profileImgInput          = document.querySelector('.user-profile #image');
-    const profileImg               = document.querySelector('.profile-img-container img');
+    const profileImgForm = document.querySelector('.user-profile .profile-img-form');
+    let profileImgInput = document.querySelector('.user-profile #image');
+    const profileImg = document.querySelector('.profile-img-container img');
 
 
     clickableText.addEventListener('click', () => openProfileImgForm());
     innerProfileImgContainer.addEventListener('click', () => openProfileImgForm());
 
-    function openProfileImgForm(){
+    function openProfileImgForm() {
       profileImgInput.click();
     };
 
@@ -24,7 +24,7 @@
   }
 
   // Scripts for the avatar submit form
-  if(document.querySelector('.create-post-form')){
+  if (document.querySelector('.create-post-form')) {
     const img = document.querySelector('#img-preview-input');
     const imgInput = document.querySelector('form.create-post-form input#image');
     const inputBtn = document.querySelector('#img-preview-input + div.btn');
@@ -32,35 +32,36 @@
     img.addEventListener('click', () => openProfileImgForm());
     inputBtn.addEventListener('click', () => openProfileImgForm());
 
-    function openProfileImgForm(){
+    function openProfileImgForm() {
       imgInput.click();
     };
 
     imgInput.addEventListener('change', () => {
-      if(imgInput.files && imgInput.files[0]){
+      if (imgInput.files && imgInput.files[0]) {
         img.setAttribute('src',
-        window.URL.createObjectURL(imgInput.files[0])
+          window.URL.createObjectURL(imgInput.files[0])
         );
       }
     });
   }
 
   // Scripts for posts
-  if(document.getElementById('post-container')){
-    const processReaction = (baseBtn, oppositeBtn, response) => {
-      if(response.result === false){
+  if (document.getElementById('post-container')) {
+    const processReaction = (baseBtn, baseAlert, oppositeBtn, response) => {
+      if (response.result === false) {
+        baseAlert.style.display = 'initial';
         return false;
       }
 
-      if(response.change === 'add'){
+      if (response.change === 'add') {
         baseBtn.querySelector('span').innerHTML++;
         return true;
 
-      }else if(response.change === 'remove'){
+      } else if (response.change === 'remove') {
         baseBtn.querySelector('span').innerHTML--;
         return true;
 
-      }else if(response.change === 'switch'){
+      } else if (response.change === 'switch') {
         baseBtn.querySelector('span').innerHTML++;
         oppositeBtn.querySelector('span').innerHTML--;
         return true;
@@ -70,45 +71,61 @@
     const post_container = document.getElementById('post-container');
     const posts = post_container.querySelectorAll('section');
 
-    posts.forEach((el) => {
-      const id = el.querySelector('.post').dataset.id;
-      const likeBtn = el.querySelector('.like');
-      const dislikeBtn = el.querySelector('.dislike');
-      const commentBtn = el.querySelector('.comment');
+    posts.forEach((post) => {
+      const id = post.querySelector('.post').dataset.id;
+      const likeBtn = post.querySelector('.like');
+      const dislikeBtn = post.querySelector('.dislike');
+      const commentBtn = post.querySelector('.comment');
 
-      const likeAlert = el.querySelector('.like-alert');
-      const dislikeAlert = el.querySelector('.dislike-alert');
-      const commentAlert = el.querySelector('.comment-alert');
+      const likeAlert = post.querySelector('.like-alert');
+      const dislikeAlert = post.querySelector('.dislike-alert');
+      const commentAlert = post.querySelector('.comment-alert');
 
 
       // When like btn is clicked
       likeBtn.addEventListener('click', (event) => {
         fetch(`/api/reaction.php?id=${id}&status=1`)
-        .then(res => res.json())
-        .then(json => {
-          processReaction(likeBtn, dislikeBtn, json);
+          .then(res => res.json())
+          .then(json => {
+            if (!processReaction(likeBtn, likeAlert, dislikeBtn, json));
 
-        });
+          });
       });
 
       // When dislike btn is clicked
       dislikeBtn.addEventListener('click', (event) => {
         fetch(`/api/reaction.php?id=${id}&status=2`)
-        .then(res => res.json())
-        .then(json => {
-          processReaction(dislikeBtn, likeBtn, json);
-        });
+          .then(res => res.json())
+          .then(json => {
+            processReaction(dislikeBtn, dislikeAlert, likeBtn, json);
+          });
       });
 
       // When comment btn is clicked
       commentBtn.addEventListener('click', (event) => {
         fetch(`/api/reaction.php?id=${id}&status=0`)
-        .then(res => res.json())
-        .then(json => {
-          console.log(json);
-          commentAlert.style.display = 'initial';
-        });
+          .then(res => res.json())
+          .then(json => {
+            console.log(json);
+            commentAlert.style.display = 'initial';
+          });
       });
+
+      // Delete btn modal
+      if (post.querySelector('button.delete')) {
+        const deleteBtn = post.querySelector('button.delete');
+        const modal = post.querySelector('.custom_modal');
+        deleteBtn.addEventListener('click', () => {
+
+          modal.style.display = 'initial';
+          post.querySelectorAll('.custom_modal .close-btn').forEach((btn) => {
+            btn.addEventListener('click', () => {
+              modal.style.display = 'none';
+            });
+          });
+        });
+      }
+
     });
   }
 })();
