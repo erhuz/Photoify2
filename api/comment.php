207d23efd3688465ad3897d4c74fdce996cd5b85
Benjamin Fransson <benjamin@cafcon.se>
2019-01-10 15:38:05 +0100

    Added test data for get comments

--- api/comment.php
+++ api/comment.php
@@ -6,10 +6,19 @@ header('Content-Type: application/json');
 
 // In this file we comment on posts and send the data back as encoded JSON.
 
-
+$stmt = true;
 if(!$stmt){
     // REMOVE ME BEFORE PRODUCTION
     die(var_dump($pdo->errorInfo()));
+}
+
+if($_GET['action'] === 'read'){
+    $comments = [
+        ['avatar' => '', 'name' => 'Benjamin Fransson', 'content' => 'This is a comment'],
+        ['avatar' => '', 'name' => 'André Broman', 'content' => 'This commen thing isn\'t really me. ']
+    ];
+
+    $data = $comments;
 }
 
 echo json_encode($data);
