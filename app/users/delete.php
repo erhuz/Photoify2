1a106513f8cce5f31bf62bf4260baf985138260d
Benjamin Fransson <benjamin@cafcon.se>
2019-01-16 16:55:46 +0100

    Added placeholder logic

--- app/users/delete.php
+++ app/users/delete.php
@@ -13,8 +13,22 @@ if(!USER_IS_LOGGEDIN){
 
 if(!isset($_POST['confirm_password'], $_POST['confirm_checkbox'])){
     set_alert('Authentication failed. Account deletion aborted!', 'danger');
-    redirect('/');
+    redirect('/account.php');
 }
+
+$password = $_POST['confirm_password'];
+
+$query = 'SELECT password FROM users WHERE id = :user_id';
+$params = [
+    ':user_id' => User['id']
+];
+
+$stmt = $pdo->prepare($query);
+$stmt->execute($params);
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+die(var_dump($result));
+
 
 // Validate password and checkbox value
     // Delete user together with posts, reactions, comments and local images
