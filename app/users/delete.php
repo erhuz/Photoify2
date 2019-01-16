12194d67fe1ea689d1e8534ac1d7faff1c77b0ef
Benjamin Fransson <benjamin@cafcon.se>
2019-01-16 16:47:18 +0100

    added alert and edited alert type

--- app/users/delete.php
+++ app/users/delete.php
@@ -7,11 +7,13 @@ require __DIR__.'/../autoload.php';
 // In this file we delete users.
 
 if(!USER_IS_LOGGEDIN){
-    set_alert('You need to be logged in to take this action.', 'danger');
+    set_alert('You need to be logged in to take this action.', 'warning');
     redirect('/');
 }
 
 if(!isset($_POST['confirm_password'], $_POST['confirm_checkbox'])){
+    set_alert('Authentication failed. Account deletion aborted!', 'danger');
+    redirect('/');
 }
 
 // Validate password and checkbox value
