a929708c1ba7263c26a5264659f64182ab91ece3
Benjamin Fransson <benjamin@cafcon.se>
2019-01-16 16:46:45 +0100

    Added delete modal to account page

--- account.php
+++ account.php
@@ -10,6 +10,44 @@
                 <h2>Account information</h2>
             </div>
         </div>
+
+        <!-- Delete-account-modal -->
+        <div class="custom_modal">
+            <div class="container mt-4 p-4 modal-content">
+                <!-- Modal content -->
+                <div class="row">
+                    <div class="col-12">
+                        <span class="close-btn close">&times;</span>
+                        <h3>Warning!</h3>
+                        <p>Are you sure you want to delete your <b>ENTIRE ACCOUNT</b>?<br>
+                            This action <b>CANNOT</b> be undone! Your account and its
+                            content will be gone <b>FOREVER</b>.</p>
+                    </div>
+                </div>
+
+                <form action="/app/users/delete.php" method="POST">
+                    <div class="row mt-4">
+                        <div class="border rounded p-4">
+                            <label for="password_confirm">Type your password to confirm</label><br>
+                            <input class="form-control" type="password" placeholder="********"><br>
+
+                            <label for="checkbox_confirm">Are you sure? Confirm: </label>
+                            <input class="" type="checkbox">
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-6">
+                            <a href="" class="btn btn-block btn-danger text-white">DELETE</a>
+                        </div>
+                        <div class="col-6">
+                            <button class="close-btn btn btn-block btn-outline-secondary">CANCEL</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div> <!-- /Delete-account-modal -->
+
         <div class="row">
             <div class="col-md-12 col-lg-8">
                 <form action="/app/users/update.php" method="POST">
@@ -60,7 +98,7 @@
             <div class="col">
                 <a href="/post/new.php" class="btn btn-primary btn-block">Create new post</a>
                 <a href="/profile.php?id=<?= User['id'] ?>" class="btn btn-primary btn-block">Manage my posts</a>
-                <a href="#" class="btn btn-danger btn-block">Delete account</a>
+                <button class="btn btn-danger btn-block delete-account">Delete account</button>
             </div>
         </div>
     </div>
